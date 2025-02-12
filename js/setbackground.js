7ecfaacf61308ffa122f833577b22722944611ac
DanielJensenKEA <daje0003@stud.kea.dk>
2025-02-12 08:47:57 +0100

    First commit

---
+++ js/setbackground.js
@@ -0,0 +1,48 @@
+let inp = document.querySelector(".inpColor");
+console.log(inp);
+
+let pbCol = document.querySelector(".pbSetColor");
+console.log(pbCol);
+
+pbCol.textContent="Tryk mig for set color";
+
+let bdy = document.querySelector("body");
+console.log(bdy);
+
+pbCol.addEventListener('click', setBackgroundColor)
+document.addEventListener('keyup', setBackgroundColor)
+
+
+let inpColorValue = document.querySelector(".inpColorValue");
+const inpColorPicker = document.getElementById("inpColorPicker")
+inpColorPicker.addEventListener('input', useColorPicker)
+const pTags = document.getElementsByTagName('p')
+console.log(pTags)
+
+const pArray = Array.from(pTags);
+pArray.forEach(increaseFont)
+
+
+function increaseFont(element) {
+    let fontSize = element.style.fontSize;
+    console.log(fontSize)
+    element.style.fontSize = 10 + 'px';
+    fontSize = element.style.fontSize;
+    console.log(fontSize);
+}
+
+function useColorPicker() {
+    let col = inpColorPicker.value;
+    console.log(col);
+    bdy.style.background = col;
+    pbCol.style.color = col;
+    inpColorValue.value = bdy.style.backgroundColor
+}
+
+function setBackgroundColor() {
+    let col = inp.value;
+    console.log(col);
+    bdy.style.background = col;
+    pbCol.style.color = col;
+    inpColorValue.value = bdy.style.backgroundColor
+}
